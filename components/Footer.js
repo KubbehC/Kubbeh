54cf7a5414d62e451fc845c46f0804700ff6132e
KubbehC <124851159+KubbehC@users.noreply.github.com>
2023-02-08 20:04:16 -0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
     <>
     <footer className={styles.footer}>
         <ul class="mylinks">
-             <li><a href="https://www.yelp.com/biz/kubbeh-cohen-woodland-hills-3" target="_blank">Yelp</a></li>
+             <li><a href="https://www.yelp.com/biz/kubbeh-cohen-woodland-hills-3" target="_blank">Check us on Yelp</a></li>
              <li><a href="https://fargosoftware.netlify.app/" target="_blank">Made by Fargo Software</a></li>
         </ul>      
     </footer>
