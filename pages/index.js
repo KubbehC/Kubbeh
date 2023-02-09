2b2c30804fbed41b160092e647b0b2dceee78c6f
KubbehC <124851159+KubbehC@users.noreply.github.com>
2023-02-08 18:42:02 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,20 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Kubbeh Cohen</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <Header title="Kubbeh Cohen" />
+        <p className="details">
+          Great Israeli and Middle Eastern food. Address: 21765 Ventura blvd. Woodland Hills, CA 91364, Phone: (747) 242-1199
         </p>
       </main>
+  
+      <body>
+        <img src="Menu.jpg" width="356" height="400"></img>
+      </body>
 
       <Footer />
     </div>
