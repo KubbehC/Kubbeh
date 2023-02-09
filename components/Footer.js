8f99ba7b507e25a3f8dca382f21d51625cff9498
KubbehC <124851159+KubbehC@users.noreply.github.com>
2023-02-08 18:29:52 -0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,17 @@
+Footer.js
+
 import styles from './Footer.module.css'
 
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
-      </footer>
+    <footer className={styles.footer}>
+        <ul class="mylinks">
+             <li><a href="https://www.yelp.com/biz/kubbeh-cohen-woodland-hills-3" target="_blank">Yelp</a></li>
+             <li><a href="https://fargosoftware.netlify.app/" target="_blank">Made by Fargo Software</a></li>
+        </ul>      
+    </footer>
     </>
   )
 }
+
